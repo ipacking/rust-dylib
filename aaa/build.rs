a1e9ce39c44dfada95d0846fd3a21b8187b2deb5
c <iclings@qq.com>
2025-03-20 22:33:08 +0800

    lib

--- aaa/build.rs
+++ aaa/build.rs
@@ -1,4 +1,13 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=target/debug");
-    println!("cargo:rustc-link-lib=dylib=bbb.dll");
+    #[cfg(target_os = "windows")]
+    {
+        println!("cargo:rustc-link-search=native=target/debug");
+        println!("cargo:rustc-link-lib=dylib=bbb.dll");
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    {
+        println!("cargo:rustc-link-search=native=target/debug");
+        println!("cargo:rustc-link-lib=dylib=bbb");
+    }
 }
